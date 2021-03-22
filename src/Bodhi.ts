ca42ee911f9473cab09c47162119198a073c1ee2
jbargu <jure.grabnar@gmail.com>
2021-03-22 13:38:24 +0100

    Bodhi setup function

--- src/Bodhi.ts
+++ src/Bodhi.ts
@@ -1,7 +1,35 @@
+import { TestAccountSigningKey, Provider, Signer } from "@acala-network/bodhi";
+import { WsProvider, Keyring } from "@polkadot/api";
+import { createTestPairs } from "@polkadot/keyring/testingPairs";
+import { KeyringPair } from "@polkadot/keyring/types";
+
 export class Bodhi {
+  WS_URL = process.env.WS_URL || "ws://127.0.0.1:9944";
+  seed = process.env.SEED;
 
   public async setup() {
-    return "test";
-  }
+    const provider = new Provider({
+      provider: new WsProvider(this.WS_URL),
+    });
 
+    await provider.api.isReady;
+
+    let pair: KeyringPair;
+    if (this.seed) {
+      const keyring = new Keyring({ type: "sr25519" });
+      pair = keyring.addFromUri(this.seed);
+    } else {
+      const testPairs = createTestPairs();
+      pair = testPairs.alice;
+    }
+
+    const signingKey = new TestAccountSigningKey(provider.api.registry);
+    signingKey.addKeyringPair(pair);
+
+    const wallet = new Signer(provider, pair.address, signingKey);
+    return {
+      wallet,
+      provider,
+    };
+  }
 }
