9641a324578245bd9d34c807ee48174c6a6ee649
Žiga Franko <franko.ziga@gmail.com>
2021-03-29 11:59:57 +0200

    proxy builder allows us to set the proxy connector on the go

---
+++ src/proxy-builder.ts
@@ -0,0 +1,16 @@
+import { HardhatPluginError } from "hardhat/plugins";
+import { HardhatRuntimeEnvironment } from "hardhat/types/runtime";
+import { BodhiProxy } from "./proxies/BodhiProxy";
+import EthersProxy from "./proxies/EthersProxy";
+import { GANATCH_CHAIN, ProxyProvider, REEF_CHAIN } from "./types";
+
+export const proxyBuilder = (chain: string, hre: HardhatRuntimeEnvironment): ProxyProvider => {
+  switch (chain) {
+    case REEF_CHAIN:
+      return new BodhiProxy()
+    case GANATCH_CHAIN:
+      return new EthersProxy(hre.ethers);
+    default:
+      throw new HardhatPluginError("Reef-provider", `Unknown chain! Avaliable: [${REEF_CHAIN}, ${GANATCH_CHAIN}]`);
+  }
+}
