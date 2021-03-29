1a09f778b81597dcaa7e2707b0088c0fe5291f1d
Žiga Franko <franko.ziga@gmail.com>
2021-03-29 12:02:06 +0200

    Test clean up... TODO!

--- test/project.test.ts
+++ test/project.test.ts
@@ -3,7 +3,7 @@ import { assert } from "chai";
 import path from "path";
 
 
-import { Bodhi } from "../src/Bodhi";
+import { BodhiProxy } from "./proxies/BodhiProxy";
 import { Provider, Signer } from "@acala-network/bodhi";
 
 import { useEnvironment } from "./helpers";
@@ -12,22 +12,6 @@ describe("Integration tests examples", function () {
   describe("Hardhat Runtime Environment extension", function () {
     useEnvironment("hardhat-project");
 
-    it("Should add the bodhi field", function () {
-      assert.instanceOf(
-        this.hre.bodhi,
-        Bodhi
-      );
-    });
   });
 
-  describe("HardhatConfig extension", function () {
-    useEnvironment("hardhat-project");
-
-    it("Should add the newPath to the config", function () {
-      assert.equal(
-        this.hre.config.paths.newPath,
-        path.join(process.cwd(), "asd")
-      );
-    });
-  });
 });
