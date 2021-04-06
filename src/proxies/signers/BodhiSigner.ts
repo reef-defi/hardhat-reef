ae1350e082c9e8703639d824dbca342ba7bf95a8
Žiga Franko <franko.ziga@gmail.com>
2021-04-06 10:35:02 +0200

    Quick fix

--- src/proxies/signers/BodhiSigner.ts
+++ src/proxies/signers/BodhiSigner.ts
@@ -1,5 +1,5 @@
 import { ReefSigner } from "./ReefSigner";
-import { Signer } from "@acala-network/bodhi";
+import { Signer } from "@reef-defi/evm-provider";
 
 // Class is never used and is defined just for type testing purposes 
 export default class BodhiSigner extends Signer implements ReefSigner { }
