db205e290662f12259577878785a1f4d8326fd0a
Žiga Franko <franko.ziga@gmail.com>
2021-04-08 12:33:00 +0200

    ReefChainService pulls in reef-node into the project and uses for running local reef chain. Some functions were cleaned

--- src/reef-chain/ReefChainService.ts
+++ src/reef-chain/ReefChainService.ts
@@ -1,15 +1,22 @@
+import fs from "fs";
+import http from "https";
+import fetch from 'node-fetch';
+
 import { ChildProcess, exec, spawn } from "child_process";
 import { throwError } from "../utils";
+
+const REEF_NODE_FILE_PATH = "./reef-node";
+const UNIX_REEF_NODE_RELEASE = "https://github.com/reef-defi/reef-chain/releases/download/testnet-2.1/reef-node";
 
 export default class ReefChainService {
   private static service: ChildProcess | undefined;
   private static killReefProcessStatements = "ps -A | grep reef-node | grep -v grep | awk '{print $1}' | xargs kill -9";
 
-  public static async createService(chainPath: string) {
-    await ReefChainService.buildChain(chainPath)
+  public static async createService() {
+    await ReefChainService.pullChain();
     return new Promise((resolve, reject) => {
       console.log("Starting Reef-node service!");
-      ReefChainService.service = exec(`cd ${chainPath} && make run`, (err) => {
+      ReefChainService.service = exec(`${REEF_NODE_FILE_PATH} --dev --tmp`, (err) => {
         if (err) {
           reject(err.message);
         }
@@ -19,7 +26,7 @@ export default class ReefChainService {
       // timeout constant is also set to some high enough value to ensure chain 
       // is up in running when the Provider is called.
       setTimeout(() => resolve(1), 3300);
-    })
+    });
   }
 
   public static async stopService() {
@@ -31,37 +38,42 @@ export default class ReefChainService {
     }
   }
 
-  private static async killReefProcess() {
-    return new Promise((resolve) => {
-      const child = exec(ReefChainService.killReefProcessStatements, (err) => {
-        if (err) {
-          throwError(err.message);
-        }
-      });
-      child.on("close", () => {
-        resolve(true);
+  private static async killReefProcess(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      exec(ReefChainService.killReefProcessStatements, (err) => {
+        err
+          ? reject(err)
+          : resolve();
       });
     })
   }
 
   // Pull reef chain from reef-chain release
-  // private pullChain() { 
-
-  //   this.buildChain();
-  //  }
-
-
-  // TODO this does not 
-  private static async buildChain(chainPath: string) {
-    console.log("Building Reef chain");
-    return new Promise((resolve, reject) => {
-      exec(`cd ${chainPath} && make build`, (error) => {
-        if (error) {
-          reject(error.message);
-        }
-        resolve(1);
-      });
-    })
+  private static async pullChain() { 
+    if (!fs.existsSync(REEF_NODE_FILE_PATH)) {
+      await downloadFile(
+        UNIX_REEF_NODE_RELEASE,
+        REEF_NODE_FILE_PATH
+      );
+    }
   }
+}
 
+const downloadFile = async (url: string, filePath: string) => {
+  console.log("Downloading reef-node...");
+  return Promise.resolve()
+    .then(() => fetch(url))
+    .then((data) => data.buffer())
+    .then((buffer) => saveBufferToFile(buffer, filePath));
+};
+
+const saveBufferToFile = async (buffer: Buffer, filePath: string): Promise<void> => {
+    console.log("Saving reef-node...");
+    return new Promise((resolve, reject) => {
+      fs.writeFile(filePath, buffer, (err) => {
+        err 
+          ? reject(err) 
+          : resolve();
+      });
+  });
 }
