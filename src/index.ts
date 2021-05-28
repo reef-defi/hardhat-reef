39825f72e608407febabc6ec4abb2a012c17a1bf
Žiga Franko <franko.ziga@gmail.com>
2021-05-28 18:16:04 +0200

    removed user path include (#18)

--- src/index.ts
+++ src/index.ts
@@ -14,24 +14,6 @@ import {
   defaultReefTestnetConfig,
 } from "./utils";
 
-extendConfig(
-  (config: HardhatConfig, userConfig: Readonly<HardhatUserConfig>) => {
-    const userPath = userConfig.paths?.newPath;
-
-    let newPath: string;
-    if (userPath === undefined) {
-      newPath = path.join(config.paths.root, "newPath");
-    } else {
-      if (path.isAbsolute(userPath)) {
-        newPath = userPath;
-      } else {
-        newPath = path.normalize(path.join(config.paths.root, userPath));
-      }
-    }
-    config.paths.newPath = newPath;
-  }
-);
-
 // Configure Reef Network Parameters
 extendConfig(
   (config: HardhatConfig, userConfig: Readonly<HardhatUserConfig>) => {
