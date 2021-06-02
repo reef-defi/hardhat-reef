2df79e722c8864482b39ccb342f040e6aa09baea
Žiga Franko <franko.ziga@gmail.com>
2021-06-02 12:57:21 +0200

    Localhost account claim (#19)
    * evm provider updated
    
    * Bodhi can now claim default accounts on localhost
    
    localhost parameter can be passed into BodhiProxy as true or false argument
    
    * Setting up localhost BodhiProxy and adding mainnet BodhiProxy

--- src/proxy-builder.ts
+++ src/proxy-builder.ts
@@ -2,13 +2,15 @@ import { HardhatRuntimeEnvironment } from "hardhat/types/runtime";
 
 import { BodhiProxy } from "./proxies/BodhiProxy";
 import EthersProxy from "./proxies/EthersProxy";
-import { ProxyProvider, REEF_CHAIN, TESTNET_REEF } from "./types";
+import { MAINNET_REEF, ProxyProvider, REEF_CHAIN, TESTNET_REEF } from "./types";
 
 export const proxyBuilder = (hre: HardhatRuntimeEnvironment): ProxyProvider => {
   switch (hre.network.name) {
     case REEF_CHAIN:
-      return new BodhiProxy(hre);
+      return new BodhiProxy(hre, true);
     case TESTNET_REEF:
+      return new BodhiProxy(hre);
+    case MAINNET_REEF:
       return new BodhiProxy(hre);
     default:
       return new EthersProxy(hre);
