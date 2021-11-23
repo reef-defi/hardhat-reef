e5ceb650a5cb08b6aedff7339093eebaa5e70a7d
Ziga Franko G <frenkie@Zigas-MacBook-Pro-2.local>
2021-11-23 13:48:14 +0100

    Types fixed and clean up

--- src/proxy-builder.ts
+++ src/proxy-builder.ts
@@ -1,17 +1,17 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types/runtime";
 
-import { BodhiProxy } from "./proxies/BodhiProxy";
+import ReefProxy from "./proxies/ReefProxy";
 import EthersProxy from "./proxies/EthersProxy";
 import { MAINNET_REEF, ProxyProvider, REEF_CHAIN, TESTNET_REEF } from "./types";
 
 export const proxyBuilder = (hre: HardhatRuntimeEnvironment): ProxyProvider => {
   switch (hre.network.name) {
     case REEF_CHAIN:
-      return new BodhiProxy(hre, true);
+      return new ReefProxy(hre, true);
     case TESTNET_REEF:
-      return new BodhiProxy(hre);
+      return new ReefProxy(hre);
     case MAINNET_REEF:
-      return new BodhiProxy(hre);
+      return new ReefProxy(hre);
     default:
       return new EthersProxy(hre);
   }
